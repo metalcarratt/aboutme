26e8feea6a07d663d8978cd2024879e4c0162843
Caleb Garratt <metal.carratt@gmail.com>
2021-02-04 14:10:18 +1300

    update email address

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -28,7 +28,7 @@
             <li><a href="https://www.linkedin.com/in/caleb-garratt-5911024a" target="_blank">LinkedIn</a></li>
             <li><a href="https://stackexchange.com/users/425917/caleb" target="_blank">Stack Exchange</a></li>
             <li><a href="https://github.com/metalcarratt/" target="_blank">GitHub</a></li>
-            <li><a href="mailto:metal.carratt@gmail.com">Email</a></li>
+            <li><a href="mailto:caleb.garratt@gmail.com">Email</a></li>
         </ul>
     </main>
 </template>
